2fc4b4d69c99322ca768f20b6a69bda4e47edd7c
david840706tw <develop.tung@gmail.com>
2020-11-29 16:51:35 +0800

    commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+confirmed = pd.read_csv('covid19_confirmed.csv')
+deaths = pd.read_csv('covid19_deaths.csv')
+recovered = pd.read_csv('covid19_recovered.csv')
+
+confirmed = confirmed.drop(['Province/State', 'Lat', 'Long'], axis=1)
+deaths = deaths.drop(['Province/State', 'Lat', 'Long'], axis=1)
+recovered = recovered.drop(['Province/State', 'Lat', 'Long'], axis=1)
+
+confirmed = confirmed.groupby(confirmed['Country/Region']).aggregate('sum')
+deaths = deaths.groupby(deaths['Country/Region']).aggregate('sum')
+recovered = recovered.groupby(recovered['Country/Region']).aggregate('sum')
+
+confirmed = confirmed.T
+deaths = deaths.T
+recovered = recovered.T
+
+# print(deaths)
+# print(recovered)
+
+new_cases = confirmed.copy()
+
+for day in range(1, len(confirmed)):
+    new_cases.iloc[day] = confirmed.iloc[day] - confirmed.iloc[day - 1]
+
+# print(new_cases.tail(10))
+# print(confirmed.tail(10))
+
+growth_rate = confirmed.copy()
+
+for day in range(1, len(confirmed)):
+    growth_rate.iloc[day] = (new_cases.iloc[day] /
+                             confirmed.iloc[day - 1]) * 100
+
+# print(growth_rate.tail(10))
+
+active_cases = confirmed.copy()
+
+for day in range(0, len(confirmed)):
+    active_cases.iloc[day] = confirmed.iloc[day] - \
+        deaths.iloc[day] - recovered.iloc[day]
+
+overall_growth_rate = confirmed.copy()
+
+for day in range(1, len(confirmed)):
+    overall_growth_rate.iloc[day] = (
+        (active_cases.iloc[day] - active_cases.iloc[day - 1]) / active_cases.iloc[day - 1]) * 100
+
+print(overall_growth_rate['Taiwan'].tail(10))
